b7f6b0a1c2d05a0c3f966c22e48bde3194f43ec2
Geo <121501867+geoCrock@users.noreply.github.com>
2023-12-20 15:08:26 +0300

    Update xcount.py

--- app/xcount.py
+++ app/xcount.py
@@ -1,12 +1,11 @@
-
 async def count_x(message: str):
 
-    # Разделение текста по переходам на новую строку
+    # Splitting text using newlines
     lines = message.split('\n')
 
-    # Подсчет количества вхождений буквы "X"
+    # Counting the number of occurrences of the letter "X"
     count_of_X = sum(line.count('X') + line.count('x') for line in lines)
     average_of_X_per_line = count_of_X / len(lines) if len(lines) > 0 else 0
 
-    # Вывод результатов
+    # Output of results
     return average_of_X_per_line
